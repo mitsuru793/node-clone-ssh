4d85dee3134ac630345cc5e75ecbe864c9b605a5
mitsuru793 <mitsuru793@gmail.com>
2024-08-25 04:13:20 +0900

    refactor: make CloneSshUrl to be companion object

--- src/command.ts
+++ src/command.ts
@@ -1,8 +1,5 @@
 import is from '@sindresorhus/is'
-import {
-	changeCloneSshUrlDomain,
-	createCloneSshUrlFromId,
-} from './model/cloneSshUrl'
+import { CloneSshUrl } from './model/cloneSshUrl'
 
 type Option = {
 	domain?: string
@@ -13,8 +10,8 @@ export function run(urlRaw: string, ops: Option = {}) {
 		throw new Error('Given url must be fill string')
 	}
 
-	const cloneSshUrl = createCloneSshUrlFromId(urlRaw)
+	const cloneSshUrl = CloneSshUrl.fromId(urlRaw)
 	// TODO: remove next line
 	ops.domain = ops.domain || 'github.com'
-	return changeCloneSshUrlDomain(cloneSshUrl, ops.domain)
+	return CloneSshUrl.changeDomain(cloneSshUrl, ops.domain)
 }
--- src/model/cloneSshUrl.ts
+++ src/model/cloneSshUrl.ts
@@ -3,27 +3,74 @@ import { trim } from '../util'
 import { RepoIdName } from './repoIdName'
 import { SshUrl } from './sshUrl'
 
-type CloneSshUrl = string
+type CloneSshUrl = string & { readonly brancd: unique symbol }
+
+type ParsedCloneSshUrl = Readonly<{
+	author: string
+	repoName: string
+}>
 
 const regexp = new RegExp('^git@github.com:([^/]+)/([^/]+).git$')
 
-// TODO: make arg domain not be optional
-export function createCloneSshUrl(
-	author: string,
-	repoName: string,
-	domain = 'github.com',
-): CloneSshUrl {
-	return `git@${domain}:${author}/${repoName}.git`
-}
+export const CloneSshUrl = {
+	// TODO: make arg domain not be optional
+	create(author: string, repoName: string, domain = 'github.com'): CloneSshUrl {
+		return `git@${domain}:${author}/${repoName}.git` as CloneSshUrl
+	},
 
-export function createCloneSshUrlFromHttp(urlRaw: string): CloneSshUrl {
-	const url = new URL(urlRaw)
-	if (!isGithubRepoHttpUrl(url)) {
-		throw new Error(`Support url is github only yet: ${url}`)
-	}
+	fromId(id: string): CloneSshUrl {
+		if (is.urlString(id)) {
+			return CloneSshUrl.fromHttp(id)
+		}
 
-	const [author, repoName] = trim(url.pathname, '/').split('/')
-	return createCloneSshUrl(author, repoName)
+		if (SshUrl.is(id)) {
+			return CloneSshUrl.fromSshUrl(id)
+		}
+
+		if (RepoIdName.parse(id)) {
+			return CloneSshUrl.fromRepoIdName(id)
+		}
+
+		throw new Error(`Unsupport format of id: ${id}`)
+	},
+
+	fromHttp(urlRaw: string): CloneSshUrl {
+		const url = new URL(urlRaw)
+		if (!isGithubRepoHttpUrl(url)) {
+			throw new Error(`Support url is github only yet: ${url}`)
+		}
+
+		const [author, repoName] = trim(url.pathname, '/').split('/')
+		return CloneSshUrl.create(author, repoName)
+	},
+
+	fromSshUrl(sshUrl: SshUrl): CloneSshUrl {
+		const { author, repoName } = SshUrl.parse(sshUrl)
+		return CloneSshUrl.create(author, repoName)
+	},
+
+	fromRepoIdName(name: RepoIdName): CloneSshUrl {
+		const [author, repoName] = RepoIdName.parse(name)
+		return CloneSshUrl.create(author, repoName)
+	},
+
+	changeDomain(url: CloneSshUrl, domain: string): CloneSshUrl {
+		const { author, repoName } = CloneSshUrl.parse(url)
+		return CloneSshUrl.create(author, repoName, domain)
+	},
+
+	parse(url: CloneSshUrl): ParsedCloneSshUrl {
+		const matched = url.match(regexp)
+		if (!matched) {
+			throw new Error(`Failed to parse CloneSshUrl: ${url}`)
+		}
+
+		const [_, author, repoName] = matched
+		assert.nonEmptyString(author)
+		assert.nonEmptyString(repoName)
+
+		return { author, repoName }
+	},
 }
 
 export function isGithubRepoHttpUrl(url: URL): boolean {
@@ -32,58 +79,4 @@ export function isGithubRepoHttpUrl(url: URL): boolean {
 		return false
 	}
 	return RepoIdName.is(url.pathname.replace(/^\//, ''))
-}
-
-export function createCloneSshUrlFromSshUrl(sshUrl: SshUrl): CloneSshUrl {
-	const { author, repoName } = SshUrl.parse(sshUrl)
-	return createCloneSshUrl(author, repoName)
-}
-
-export function createCloneSshUrlFromRepoIdName(name: RepoIdName): CloneSshUrl {
-	const [author, repoName] = RepoIdName.parse(name)
-	return createCloneSshUrl(author, repoName)
-}
-
-export function createCloneSshUrlFromId(id: string): CloneSshUrl {
-	if (is.urlString(id)) {
-		return createCloneSshUrlFromHttp(id)
-	}
-
-	if (SshUrl.is(id)) {
-		return createCloneSshUrlFromSshUrl(id)
-	}
-
-	if (RepoIdName.parse(id)) {
-		return createCloneSshUrlFromRepoIdName(id)
-	}
-
-	throw new Error(`Unsupport format of id: ${id}`)
-}
-
-export function changeCloneSshUrlDomain(
-	url: CloneSshUrl,
-	domain: string,
-): CloneSshUrl {
-	const { author, repoName } = parseCloneSshUrl(url)
-	return createCloneSshUrl(author, repoName, domain)
-}
-
-type ParsedCloneSshUrl = {
-	author: string
-	repoName: string
-}
-
-function parseCloneSshUrl(url: CloneSshUrl): ParsedCloneSshUrl {
-	const regexp = new RegExp('^git@github.com:([^/]+)/([^/]+).git$')
-
-	const matched = url.match(regexp)
-	if (!matched) {
-		throw new Error(`Failed to parse CloneSshUrl: ${url}`)
-	}
-
-	const [_, author, repoName] = matched
-	assert.nonEmptyString(author)
-	assert.nonEmptyString(repoName)
-
-	return { author, repoName }
 }
